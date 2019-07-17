a217febbd207366bfca15c2a593a559f6ae5e760
Felix Bahr <felix.bahr@sevenval.com>
2019-07-17 15:14:15 +0200

    Fix imports

--- elastalert/test_rule.py
+++ elastalert/test_rule.py
@@ -14,13 +14,13 @@ import sys
 
 import mock
 
-from .elastalert.config import load_conf
-from .elastalert.elastalert import ElastAlerter
-from .elastalert.util import EAException
-from .elastalert.util import elasticsearch_client
-from .elastalert.util import lookup_es_key
-from .elastalert.util import ts_now
-from .elastalert.util import ts_to_dt
+from .config import load_conf
+from .elastalert import ElastAlerter
+from .util import EAException
+from .util import elasticsearch_client
+from .util import lookup_es_key
+from .util import ts_now
+from .util import ts_to_dt
 
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger('elasticsearch').setLevel(logging.WARNING)
